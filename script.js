512ebb0198007fab0ac662427f5036933e1c442e
mike wazowski <mike.waza@winwin.com>
2023-12-14 15:42:53 +0100

    end of lesson 14/12/23

--- script.js
+++ script.js
@@ -46,14 +46,30 @@ export function exercise04(args) {
 
 export function exercise05(args) {
   const input = args
-  let count = 0
 
-  for (let i = 1; i < input.length; i++) {
+  for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
     if (
       currentElement.charCodeAt() >= 65 &&
       currentElement.charCodeAt() <= 90
     ) {
+      return true
     }
   }
+  return false
+}
+
+export function exercise06(args) {
+  const input = args.toUpperCase()
+
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+    if (
+      (currentElement.charCodeAt() < 65 || currentElement.charCodeAt() > 90) &&
+      currentElement.charCodeAt() != 32
+    ) {
+      return true
+    }
+  }
+  return false
 }
